32077748da9d54a2774f60439b341ecb78ef104a
Mosipa <jiaric@hotmail.com>
2021-10-01 22:17:48 +0800

    Updated main.py
    Added a few more imports

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 from gui import Ui_MainWindow
 
 if __name__ == "__main__":
     import sys
-    app = QApplication(sys.argv)
-    MainWindow = QMainWindow()
+
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
 
-    ui.setupEvents()
+    setupEvents(ui)
 
     MainWindow.show()
     sys.exit(app.exec_())
