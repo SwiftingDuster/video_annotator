b8861724f343c373664f40aeb634c5eb7486b9dd
Medioec <2103020@sit.singaporetech.edu.sg>
2021-10-06 08:29:22 +0800

    Added interagreement.py for calculating inter annotator agreement. Dummy xml files test1.xml, test2.xml included.

--- interagreement.py
+++ interagreement.py
@@ -7,41 +7,48 @@ import xml.etree.ElementTree as ET
 # Important: add code to bring in xml files using UI (probably in main.py) #
 ############################################################################
 
+# Initialize xmlCalc() class with input of a list of xml
+# .computeGamma() to calculate inter-annotator agreement
+# .annotationList to return list of annotations
+class xmlCalc:
+    def __init__(self, inList):
+        self.annotationList = self.xml_to_list(inList)
+
+    # Make function to parse xml files, return list of elements of ["annotator", frame start, frame end]
+    def xml_to_list(self, xmlFileIn):
+        annotationList = []
+        j = 1
+        fstartstring = f's{j}Start'
+        fendstring = f's{j}End'
+        for i in range(len(xmlFileIn)):
+            tree = ET.parse(xmlFileIn[i])
+            root = tree.getroot()
+            annotator = root.find("annotator").text
+            for framedata in root.iter("framedata"):
+                k = 0
+                for f in framedata.iter():
+                    if f != framedata:
+                        k += 1
+                for l in range(k):
+                    framestart = int(framedata.find(fstartstring).text)
+                    frameend = int(framedata.find(fendstring).text)
+                    annotationList.append([annotator, f"Annotation {j}", framestart, frameend])
+                    j += 1
+        return annotationList
+
+    # Function to calculate gamma for inter-annotator agreement. Gamma closer to 1 = higher agreement
+    def computeGamma(self):
+        annotationList = self.annotationList
+        continuum = Continuum()  # add annotation data to continuum
+        for x in annotationList:
+            continuum.add(x[0], Segment(x[2], x[3]), "")
+        dissim = PositionalSporadicDissimilarity(delta_empty=1.0)
+        gamma_results = continuum.compute_gamma(dissim)  # output final gamma val
+        # print(f"The gamma for that annotation is f{gamma_results.gamma}") old code
+        return gamma_results.gamma
 
 xmlList = ["test1.xml", "test2.xml"]  # Store xml file names/dir as strings in list
-
-# Make function to parse xml files, return list of ["annotator", frame start, frame end]
-def xml_to_pygammaIn(xmlFileIn):
-    annotationList = []
-    j = 1
-    framestart = f's{j}Start'
-    frameend = f's{j}End'
-    for i in range(len(xmlFileIn)):
-        tree = ET.parse(xmlFileIn[i])
-        root = tree.getroot()
-        annotator = root.find("annotator").text
-        for framedata in root.iter("framedata"):
-            k = 0
-            for f in framedata.iter():
-                if f != framedata:
-                    k += 1
-            for l in range(k):
-                annotationList.append([annotator, int(framedata.find(framestart).text), int(framedata.find(frameend).text)])
-                j += 1
-    return annotationList
-
-# Function to calculate gamma for inter-annotator agreement. Gamma closer to 1 = higher agreement
-def compute_Gamma(annotationList):
-    continuum = Continuum()  # add annotation data to continuum
-    for x in annotationList:
-        continuum.add(x[0], Segment(x[1], x[2]), "")
-    dissim = PositionalSporadicDissimilarity(delta_empty=1.0)
-    gamma_results = continuum.compute_gamma(dissim)  # output final gamma val
-    # print(f"The gamma for that annotation is f{gamma_results.gamma}") old code
-    return gamma_results.gamma
-
-
 # Example print for illustration
-annotationList = xml_to_pygammaIn(xmlList)
-print(annotationList)
-print(compute_Gamma(annotationList))
+xmlData = xmlCalc(xmlList)
+print(xmlData.annotationList)
+print(xmlData.computeGamma())
