3c8f39fc9d3a21aff34b2eeb724bf4cca41bc3b9
Kagurawei <weicheng4a12014@gmail.com>
2021-10-08 21:28:49 +0800

    Successfully changed function GenerateXML
    changed GenerateXML to accept changes in element name and element value in xml file structure, according to input.
    
    created modifyXML to change one field value in the specified xml element

--- xmlHandler.py
+++ xmlHandler.py
@@ -20,10 +20,10 @@ class XMLhandler:
 
 # create an xml object to store xml stuff
 
-    def addFramedata(self):
-
-        tree=ET.parse(self.filename)
-        root=tree.getroot()
+    # def addFramedata(self):
+    #
+    #     tree=ET.parse(self.filename)
+    #     root=tree.getroot()
 
         # for framedata in root.iter(self.element):
         #     k = 0
@@ -38,55 +38,109 @@ class XMLhandler:
 
 
 
-    def GenerateXML(self):
+    def GenerateXML(self,root_name,annotator_name,annotator_value,foldername,
+                    foldername_value,source,database_name,database_value,
+                    size,width,width_value,
+                    height,height_value,depth,depth_value,
+                    video_resolution,video_resolution_value,
+                    segmented,segmented_value,object,
+                    name,name_value,pose,pose_value,truncated,
+                    truncated_value,difficult,difficult_value,
+                    boundbox,xmin,xmin_value,ymin,ymin_value,
+                    xmax,xmax_value,ymax,ymax_value,framedata,
+                    s1Start,s1Start_value,s1End,s1End_value):
     # create the file structure
-        root = ET.Element('annotation')
-        annotator = ET.SubElement(root, 'annotator')
-        annotator.text = 'Name1'
-        folderName = ET.SubElement(root, "folder")
-        folderName.text = "n00007846"
-        source = ET.SubElement(root, "source")
-        database = ET.SubElement(source, "database")
-        database.text = "ImageNet database"
-        size = ET.SubElement(root, "size")
-        width = ET.SubElement(size, "width")
-        width.text = "500"
-        height = ET.SubElement(size, "height")
-        height.text = "333"
-        depth = ET.SubElement(size, "depth")
-        depth.text = "3"
-        video_resolution=ET.SubElement(root,"video_resolution")
-        video_resolution.text="1920x1080"
-        segmented = ET.SubElement(root, "segmented")
-        segmented.text = "0"
-        object = ET.SubElement(root, "object")
-        name = ET.SubElement(object, "name")
-        name.text = "n0007846"
-        pose = ET.SubElement(object, "pose")
-        pose.text = "Unspecified"
-        truncated = ET.SubElement(object, "truncated")
-        truncated.text = "0"
-        difficult = ET.SubElement(object, "difficult")
-        difficult.text = "0"
-        boundbox = ET.SubElement(object, "bndbox")
-        xmin = ET.SubElement(boundbox, "xmin")
-        xmin.text = "161"
-        ymin = ET.SubElement(boundbox, "ymin")
-        ymin.text = "52"
-        xmax = ET.SubElement(boundbox, "xmax")
-        xmax.text = "285"
-        ymax = ET.SubElement(boundbox, "ymax")
-        ymax.text = "247"
 
-        framedata = ET.SubElement(object, "framedata")
-        s1Start = ET.SubElement(framedata, "s1Start")
-        s1Start.text = "582"
-        s1End = ET.SubElement(framedata, "s1End")
-        s1End.text = "400"
-        s2Start = ET.SubElement(framedata, "s2Start")
-        s2Start.text = "7393"
-        s2End = ET.SubElement(framedata, "s2End")
-        s2End.text = "81034"
+        self.root_name=root_name
+        self.annotator_name=annotator_name
+        self.annotator_value=annotator_value
+        self.foldername=foldername
+        self.foldername_value=foldername_value
+        self.source=source
+        self.database_name=database_name
+        self.database_value=database_value
+        self.size=size
+        self.width=width
+        self.width_value=width_value
+        self.height=height
+        self.height_value=height_value
+        self.depth=depth
+        self.depth_value=depth_value
+        self.video_resolution=video_resolution
+        self.video_resolution_value=video_resolution_value
+        self.segmented=segmented
+        self.segmented_value=segmented_value
+        self.object=object
+        self.name=name
+        self.name_value=name_value
+        self.pose=pose
+        self.pose_value=pose_value
+        self.truncated=truncated
+        self.truncated_value=truncated_value
+        self.difficult=difficult
+        self.difficult_value=difficult_value
+        self.boundbox=boundbox
+        self.xmin=xmin
+        self.xmin_value=xmin_value
+        self.ymin=ymin
+        self.ymin_value=ymin_value
+        self.xmax=xmax
+        self.xmax_value=xmax_value
+        self.ymax=ymax
+        self.ymax_value=ymax_value
+        self.framedata=framedata
+        self.s1Start=s1Start
+        self.s1Start_value=s1Start_value
+        self.s1End=s1End
+        self.s1End_value=s1End_value
+
+
+
+
+        root = ET.Element(self.root_name)
+        annotator = ET.SubElement(root, self.annotator_name)
+        annotator.text = self.annotator_value
+        folderName = ET.SubElement(root, self.foldername)
+        folderName.text = self.foldername_value
+        source = ET.SubElement(root, self.source)
+        database = ET.SubElement(source, self.database_name)
+        database.text = self.database_value
+        size = ET.SubElement(root, self.size)
+        width = ET.SubElement(size, self.width)
+        width.text = self.width_value
+        height = ET.SubElement(size, self.height)
+        height.text = self.height_value
+        depth = ET.SubElement(size, self.depth)
+        depth.text = self.depth_value
+        video_resolution=ET.SubElement(root,self.video_resolution)
+        video_resolution.text=self.video_resolution_value
+        segmented = ET.SubElement(root, self.segmented)
+        segmented.text = self.segmented_value
+        object = ET.SubElement(root, self.object)
+        name = ET.SubElement(object, self.name)
+        name.text = self.name_value
+        pose = ET.SubElement(object, self.pose)
+        pose.text = self.pose_value
+        truncated = ET.SubElement(object, self.truncated)
+        truncated.text = self.truncated_value
+        difficult = ET.SubElement(object, self.difficult)
+        difficult.text = self.difficult_value
+        boundbox = ET.SubElement(object, self.boundbox)
+        xmin = ET.SubElement(boundbox, self.xmin)
+        xmin.text = self.xmin_value
+        ymin = ET.SubElement(boundbox, self.ymin)
+        ymin.text = self.ymin_value
+        xmax = ET.SubElement(boundbox, self.xmax)
+        xmax.text = self.xmax_value
+        ymax = ET.SubElement(boundbox, self.ymax)
+        ymax.text = self.ymax_value
+
+        framedata = ET.SubElement(object, self.framedata)
+        s1Start = ET.SubElement(framedata, self.s1Start)
+        s1Start.text = self.s1Start_value
+        s1End = ET.SubElement(framedata, self.s1End)
+        s1End.text = self.s1End_value
+
 
         # prettify the xml data
         tree = ET.ElementTree(root)
@@ -96,14 +150,24 @@ class XMLhandler:
 if __name__ == "__main__":
     #this is modify the xml: filename,element,data that you want to set
     #example: modifying foldername to file1
-    xmlinput=XMLhandler("testtest.xml","folder","file1")
+    xmlinput=XMLhandler("testtest.xml","folder","test")
 
     # if you want to generate a XML file structure
     # with dummy data, use the function below
-    # xmlinput.GenerateXML()
+    xmlinput.GenerateXML("annotation","annotator","nameless","folder",
+                    "File2","source","database","ImageNet database",
+                    "size","width","200",
+                    "height","222","depth","2",
+                    "video_resolution","1920x1020",
+                    "segmented","0","object",
+                    "name","testname","pose","Unspecified","truncated",
+                    "0","difficult","0",
+                    "boundbox","xmin","160","ymin","50",
+                    "xmax","200","ymax","130","framedata",
+                    "s1Start","10","s1End","200")
 
-    #calling teh modifyXML to modify the xml element data
-    xmlinput.modifyXML()
+    #calling the modifyXML to modify the xml element data
+    #xmlinput.modifyXML()
 
 
 
