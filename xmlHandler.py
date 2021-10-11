4d7148463135ac191a9a8f55954e707e4a9f90c4
Kagurawei <weicheng4a12014@gmail.com>
2021-10-11 21:33:20 +0800

    Revert "Updated GenerateXML function"
    This reverts commit e83d07152b2c6b5f53a515032bb9248296c89695.

--- xmlHandler.py
+++ xmlHandler.py
@@ -3,16 +3,13 @@ from xml.dom import minidom
 
 class XMLhandler:
 
-    def __init__(self,filename):
+    def __init__(self,filename,element,data):
 
         self.filename=filename
+        self.element=element
+        self.data=data
 
-
-    #accepts element name and element value to be changed
-
-    def modifyXML(self,element,data):
-        self.element = element
-        self.data = data
+    def modifyXML(self):
         tree = ET.parse(self.filename)
 
         root = tree.getroot()
@@ -23,97 +20,73 @@ class XMLhandler:
 
 # create an xml object to store xml stuff
 
+    def addFramedata(self):
+
+        tree=ET.parse(self.filename)
+        root=tree.getroot()
+
+        # for framedata in root.iter(self.element):
+        #     k = 0
+        #     for f in framedata.iter():
+        #         if f != framedata:
+        #             k+=1
+        #         for i in range(k):
+        #             ET.SubElement(root,f"s{i}Start")
+
+    # def addData(self):
+    #
 
 
 
-    def GenerateXML(self,annotator_value,
-                    foldername_value,database_value,
-                    width_value,height_value,depth_value,
-                    video_resolution_value,
-                    segmented_value,
-                    name_value,pose_value,
-                    truncated_value,difficult_value,
-                    xmin_value,ymin_value,
-                    xmax_value,ymax_value,
-                    s1Start_value,s1End_value):
+    def GenerateXML(self):
     # create the file structure
-
-        self.annotator_value=annotator_value
-        self.foldername_value=foldername_value
-        self.database_value=database_value
-        self.width_value=width_value
-        self.height_value=height_value
-        self.depth_value=depth_value
-        self.video_resolution_value=video_resolution_value
-        self.segmented_value=segmented_value
-        self.name_value=name_value
-        self.pose_value=pose_value
-        self.truncated_value=truncated_value
-        self.difficult_value=difficult_value
-        self.xmin_value=xmin_value
-        self.ymin_value=ymin_value
-        self.xmax_value=xmax_value
-        self.ymax_value=ymax_value
-        self.s1Start_value=s1Start_value
-        self.s1End_value=s1End_value
-
-
-
-
-        root = ET.Element("annotation")
-        annotator = ET.SubElement(root,"annotator")
-        annotator.text = self.annotator_value
+        root = ET.Element('annotation')
+        annotator = ET.SubElement(root, 'annotator')
+        annotator.text = 'Name1'
         folderName = ET.SubElement(root, "folder")
-        folderName.text = self.foldername_value
-        file_name = ET.SubElement(root, "filename")
-        file_name.text=self.filename
+        folderName.text = "n00007846"
         source = ET.SubElement(root, "source")
         database = ET.SubElement(source, "database")
-        database.text = self.database_value
+        database.text = "ImageNet database"
         size = ET.SubElement(root, "size")
         width = ET.SubElement(size, "width")
-        width.text = self.width_value
+        width.text = "500"
         height = ET.SubElement(size, "height")
-        height.text = self.height_value
+        height.text = "333"
         depth = ET.SubElement(size, "depth")
-        depth.text = self.depth_value
+        depth.text = "3"
         video_resolution=ET.SubElement(root,"video_resolution")
-        video_resolution.text=self.video_resolution_value
-
-        #get list of segments
-        for item in range(len(self.segmented_value)):
-            segmented = ET.SubElement(root, f"segmented{item+1}")
-            segmented.text = str(self.segmented_value[item])
+        video_resolution.text="1920x1080"
+        segmented = ET.SubElement(root, "segmented")
+        segmented.text = "0"
         object = ET.SubElement(root, "object")
         name = ET.SubElement(object, "name")
-        name.text = self.name_value
+        name.text = "n0007846"
         pose = ET.SubElement(object, "pose")
-        pose.text = self.pose_value
+        pose.text = "Unspecified"
         truncated = ET.SubElement(object, "truncated")
-        truncated.text = self.truncated_value
+        truncated.text = "0"
         difficult = ET.SubElement(object, "difficult")
-        difficult.text = self.difficult_value
-        boundbox = ET.SubElement(object, "boundbox")
+        difficult.text = "0"
+        boundbox = ET.SubElement(object, "bndbox")
         xmin = ET.SubElement(boundbox, "xmin")
-        xmin.text = self.xmin_value
+        xmin.text = "161"
         ymin = ET.SubElement(boundbox, "ymin")
-        ymin.text = self.ymin_value
+        ymin.text = "52"
         xmax = ET.SubElement(boundbox, "xmax")
-        xmax.text = self.xmax_value
+        xmax.text = "285"
         ymax = ET.SubElement(boundbox, "ymax")
-        ymax.text = self.ymax_value
+        ymax.text = "247"
+
         framedata = ET.SubElement(object, "framedata")
-        #returns list of start frames and end frames
-        for item in range(len(self.s1Start_value)):
-            s1Start = ET.SubElement(framedata, f"s{item+1}Start")
-            s1Start.text = str(self.s1Start_value[item])
-            s1End = ET.SubElement(framedata, f"s{item+1}End")
-            s1End.text = str(self.s1End_value[item])
-
-
-
-
-
+        s1Start = ET.SubElement(framedata, "s1Start")
+        s1Start.text = "582"
+        s1End = ET.SubElement(framedata, "s1End")
+        s1End.text = "400"
+        s2Start = ET.SubElement(framedata, "s2Start")
+        s2Start.text = "7393"
+        s2End = ET.SubElement(framedata, "s2End")
+        s2End.text = "81034"
 
         # prettify the xml data
         tree = ET.ElementTree(root)
@@ -121,26 +94,16 @@ class XMLhandler:
         tree.write(self.filename, encoding="utf-8")
 
 if __name__ == "__main__":
+    #this is modify the xml: filename,element,data that you want to set
     #example: modifying foldername to file1
-    xmlinput=XMLhandler("testtest.xml")
+    xmlinput=XMLhandler("testtest.xml","folder","file1")
 
     # if you want to generate a XML file structure
     # with dummy data, use the function below
-    #changed segmented to get a list
-    seg_list = ["data1", "data2",
-                  "data3", "data4", "data5"]
-    startFramelist=["10","100","20","235"]
-    endFramelist=["12","15","22","33"]
-    xmlinput.GenerateXML("nameless",
-                    "File2","ImageNet database","200",
-                    "222","2","1920x1020",
-                    seg_list,"obj1","Unspecified",
-                    "0","0","160","50","200","130",startFramelist,endFramelist)
+    # xmlinput.GenerateXML()
 
-    #calling the modifyXML to modify the xml element data
-    #modifying width to 100
-    #format: element_name,element_value
-    xmlinput.modifyXML("width","100")
+    #calling teh modifyXML to modify the xml element data
+    xmlinput.modifyXML()
 
 
 
